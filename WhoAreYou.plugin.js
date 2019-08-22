4a8085ede355555d5c8ed604218ccc87c69413ae
Ash Wolford <piro@live.com>
2019-08-22 03:44:09 -0500

    fixed version

--- WhoAreYou.plugin.js
+++ WhoAreYou.plugin.js
@@ -24,7 +24,7 @@
 @else@*/
 
 var WhoAreYou = (() => {
-    const config = {"info":{"name":"WhoAreYou","authors":[{"name":"Chami","discord_id":"165709167095578625","github_username":"planetarian","twitter_username":"pir0zhki"}],"version":"0.1.1","description":"Shows user names next to nicks in chat.","github":"https://github.com/planetarian/BetterDiscordPlugins","github_raw":"https://github.com/planetarian/BetterDiscordPlugins/blob/master/WhoAreYou.plugin.js"},"changelog":[{"title":"0.1.1","items":["Fixed a bug where existing messages in channels wouldn't get updated."]},{"title":"Initial release","items":["I did a thing"]}],"main":"index.js"};
+    const config = {"info":{"name":"WhoAreYou","authors":[{"name":"Chami","discord_id":"165709167095578625","github_username":"planetarian","twitter_username":"pir0zhki"}],"version":"0.2","description":"Shows user names next to nicks in chat.","github":"https://github.com/planetarian/BetterDiscordPlugins","github_raw":"https://github.com/planetarian/BetterDiscordPlugins/blob/master/WhoAreYou.plugin.js"},"changelog":[{"title":"0.2","items":["Fixed a bug where existing messages in channels wouldn't get updated."]},{"title":"Initial release","items":["I did a thing"]}],"main":"index.js"};
 
     return !global.ZeresPluginLibrary ? class {
         constructor() {this._config = config;}
