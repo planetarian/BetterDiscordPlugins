5f48997cae7c7304398be230ca61d3a5c22fb353
Ash Wolford <piro@live.com>
2020-01-15 22:43:05 -0600

    New plugin format, fixed new emote priority issue
    Previously, BTTV2 and FFZ emote priorities were swapped. This time, BTTV and FFZ were swapped. This new version takes care of the issue, while also updating to the new plugin format.

--- BDEmotesFFZPrioritizer.plugin.js
+++ BDEmotesFFZPrioritizer.plugin.js
@@ -1,111 +1,99 @@
-//META{"name":"BDEmotesFFZPrioritizer","website":"https://github.com/planetarian/BetterDiscordPlugins","source":"https://github.com/planetarian/BetterDiscordPlugins/blob/master/BDEmotesFFZPrioritizer.plugin.js"}*//
-
+//META{"name":"BDEmotesFFZPrioritizer","displayName":"BDEmotesFFZPrioritizer","website":"https://github.com/planetarian/BetterDiscordPlugins","source":"https://raw.githubusercontent.com/planetarian/BetterDiscordPlugins/master/BDEmotesFFZPrioritizer.plugin.js"}*//
 /*@cc_on
 @if (@_jscript)
-    
-    // Offer to self-install for clueless users that try to run this directly.
-    var shell = WScript.CreateObject("WScript.Shell");
-    var fs = new ActiveXObject("Scripting.FileSystemObject");
-    var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
-    var pathSelf = WScript.ScriptFullName;
-    // Put the user at ease by addressing them in the first person
-    shell.Popup("It looks like you tried to run me directly. This is not desired behavior! It will work now, but likely will not work with other plugins. Even worse, with other untrusted plugins it may lead computer virus infection!", 0, "I'm a plugin for BetterDiscord", 0x30);
-    if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
-        shell.Popup("I'm in the correct folder already.\nJust reload Discord with Ctrl+R.", 0, "I'm already installed", 0x40);
-    } else if (!fs.FolderExists(pathPlugins)) {
-        shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
-    } else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
-        fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
-        // Show the user where to put plugins in the future
-        shell.Exec("explorer " + pathPlugins);
-        shell.Popup("I'm installed!\nJust reload Discord with Ctrl+R.", 0, "Successfully installed", 0x40);
-    }
-    WScript.Quit();
+	
+	// Offer to self-install for clueless users that try to run this directly.
+	var shell = WScript.CreateObject("WScript.Shell");
+	var fs = new ActiveXObject("Scripting.FileSystemObject");
+	var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\BetterDiscord\plugins");
+	var pathSelf = WScript.ScriptFullName;
+	// Put the user at ease by addressing them in the first person
+	shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+	if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+		shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
+	} else if (!fs.FolderExists(pathPlugins)) {
+		shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+	} else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+		fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
+		// Show the user where to put plugins in the future
+		shell.Exec("explorer " + pathPlugins);
+		shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
+	}
+	WScript.Quit();
 
-@else @*/
+@else@*/
 
-class BDEmotesFFZPrioritizer {
-    getName() { return "BDEmotesFFZPrioritizer"; }
-    getDescription() { return "Ensures that FFZ emotes have priority over the objectively inferior BTTV emotes. Get your Klappa on."; }
-    getVersion() { return "0.0.7"; }
-    getAuthor() { return "Chami"; }
-    getSettingsPanel() { return "<h3>BDEmotesFFZPrioritizer Settings</h3>"; }
+var BDEmotesFFZPrioritizer = (() => {
+    const config = {"info":{"name":"BDEmotesFFZPrioritizer","authors":[{"name":"Chami","discord_id":"165709167095578625","github_username":"planetarian","twitter_username":"pir0zhki"}],"version":"0.1.0","description":"Ensures that FFZ emotes have priority over the objectively inferior BTTV emotes. Get your Klappa on.","github":"https://github.com/planetarian/BetterDiscordPlugins","github_raw":"https://raw.githubusercontent.com/planetarian/BetterDiscordPlugins/master/BDEmotesFFZPrioritizer.plugin.js"},"changelog":[{"title":"0.1.0","items":["Moved to new BD plugin format"]}],"main":"index.js"};
 
-    constructor() {
-    }
-    
-    load(){ this.log("Loaded"); }
+    return !global.ZeresPluginLibrary ? class {
+        constructor() {this._config = config;}
+        getName() {return config.info.name;}
+        getAuthor() {return config.info.authors.map(a => a.name).join(", ");}
+        getDescription() {return config.info.description;}
+        getVersion() {return config.info.version;}
+        load() {
+            const title = "Library Missing";
+            const ModalStack = BdApi.findModuleByProps("push", "update", "pop", "popWithKey");
+            const TextElement = BdApi.findModuleByProps("Sizes", "Weights");
+            const ConfirmationModal = BdApi.findModule(m => m.defaultProps && m.key && m.key() == "confirm-modal");
+            if (!ModalStack || !ConfirmationModal || !TextElement) return BdApi.alert(title, `The library plugin needed for ${config.info.name} is missing.<br /><br /> <a href="https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js" target="_blank">Click here to download the library!</a>`);
+            ModalStack.push(function(props) {
+                return BdApi.React.createElement(ConfirmationModal, Object.assign({
+                    header: title,
+                    children: [TextElement({color: TextElement.Colors.PRIMARY, children: [`The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`]})],
+                    red: false,
+                    confirmText: "Download Now",
+                    cancelText: "Cancel",
+                    onConfirm: () => {
+                        require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (error, response, body) => {
+                            if (error) return require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
+                            await new Promise(r => require("fs").writeFile(require("path").join(ContentManager.pluginsFolder, "0PluginLibrary.plugin.js"), body, r));
+                        });
+                    }
+                }, props));
+            });
+        }
+        start() {}
+        stop() {}
+    } : (([Plugin, Api]) => {
+        const plugin = (Plugin, Library) => {
 
-    start(){
-        this.log("Starting");
-        let libraryScript = document.getElementById('zeresLibraryScript');
-        if (!libraryScript || (window.ZeresLibrary && window.ZeresLibrary.isOutdated)) {
-            if (libraryScript) libraryScript.parentElement.removeChild(libraryScript);
-            libraryScript = document.createElement("script");
-            libraryScript.setAttribute("type", "text/javascript");
-            libraryScript.setAttribute("src", "https://rauenzi.github.io/BetterDiscordAddons/Plugins/PluginLibrary.js");
-            libraryScript.setAttribute("id", "zeresLibraryScript");
-            document.head.appendChild(libraryScript);
+    const { Logger, Patcher, Settings } = Library;    
+
+    return class BDEmotesFFZPrioritizer extends Plugin {
+        constructor() {
+            super();
         }
 
-        this.initialized = false;
-        if (window.ZeresLibrary) this.initialize();
-        else libraryScript.addEventListener("load", () => { this.initialize(); });
-        // Fallback in case load fails to fire (https://github.com/planetarian/BetterDiscordPlugins/issues/2)
-        setTimeout(this.initialize.bind(this), 5000);
-
-        this.fixEmotes();
-        
-    }
-
-    initialize(){
-        if (this.initialized) return;
-        this.initialized = true;
-
-        try {
-            PluginUtilities.checkForUpdate(this.getName(), this.getVersion(),
-                "https://raw.githubusercontent.com/planetarian/BetterDiscordPlugins/master/BDEmotesFFZPrioritizer.plugin.js");
+        onStart() {
+            Logger.log("Started");
+            this.fixEmotes();
         }
-        catch (err) {
-            this.error("Couldn't update plugin");
+
+        onStop() {
+            this.fixEmotes(false);
+            Patcher.unpatchAll();
+            Logger.log("Stopped");
         }
-        this.log("Initialized");
-    }
 
-    stop() {
-        this.fixEmotes(false);
-        this.log("Stopped");
-    }
-    unload() { this.log("Unloaded"); }
-
-    log(text) {
-        return console.log(`[%c${this.getName()}%c] ${text}`,
-            'color: #F77; text-shadow: 0 0 1px black, 0 0 2px black, 0 0 3px black;', '');
-    }
-
-    error(text) {
-        try {
-            PluginUtilities.showToast(`[${this.getName()}] Error: ${text}`, {type:'error'});
+        onSwitch() {
+            this.fixEmotes();
         }
-        catch (err) {}
-        return console.error(`[%c${this.getName()}%c] ${text}`,
-            'color: #F77; text-shadow: 0 0 1px black, 0 0 2px black, 0 0 3px black;', '');
-    }
 
-    // check on switch, in case BD updates emotes file while client is running
-    onSwitch() { this.fixEmotes(); }
-
-    // doFix: true to swap the repos, false to put them back to default
-    fixEmotes(doFix = true) {
-        //doFix = false; // remove this to reactivate the plugin
-        let ffz = window.bdEmotes.FrankerFaceZ;
-        if (ffz && ffz.Klappa && ffz.Klappa.startsWith('https://cdn.frankerfacez.com/') == doFix) {
-            window.bdEmotes.FrankerFaceZ = window.bdEmotes.BTTV2;
-            window.bdEmotes.BTTV2 = ffz;
-            this.log("Emotes swapped" + (doFix ? "." : " back."));
+        // doFix: true to swap the repos, false to put them back to default
+        fixEmotes(doFix = true) {
+            let ffz = window.bdEmotes.FrankerFaceZ;
+            if (ffz && ffz.Klappa && ffz.Klappa.startsWith('https://cdn.frankerfacez.com/') == doFix) {
+                window.bdEmotes.FrankerFaceZ = window.bdEmotes.BTTV;
+                window.bdEmotes.BTTV = ffz;
+                Logger.log("Emotes swapped" + (doFix ? "." : " back."));
+            }
         }
-    }
+    };
 
-}
-
-/*@end @*/
+};
+        return plugin(Plugin, Api);
+    })(global.ZeresPluginLibrary.buildPlugin(config));
+})();
+/*@end@*/       
