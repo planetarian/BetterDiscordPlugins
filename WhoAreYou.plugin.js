902647ce1a42322e23e88934b5198b53da85870f
Ash Wolford <piro@live.com>
2020-02-04 19:21:40 -0600

    0.3.6: further fixes for new discord update

--- WhoAreYou.plugin.js
+++ WhoAreYou.plugin.js
@@ -24,7 +24,7 @@
 @else@*/
 
 var WhoAreYou = (() => {
-    const config = {"info":{"name":"WhoAreYou","authors":[{"name":"Chami","discord_id":"165709167095578625","github_username":"planetarian","twitter_username":"pir0zhki"}],"version":"0.3.5","description":"Shows user names next to nicks in chat.","github":"https://github.com/planetarian/BetterDiscordPlugins","github_raw":"https://raw.githubusercontent.com/planetarian/BetterDiscordPlugins/master/WhoAreYou.plugin.js"},"changelog":[{"title":"0.3.5","items":["Fix for new discord update"]}],"main":"index.js"};
+    const config = {"info":{"name":"WhoAreYou","authors":[{"name":"Chami","discord_id":"165709167095578625","github_username":"planetarian","twitter_username":"pir0zhki"}],"version":"0.3.6","description":"Shows user names next to nicks in chat.","github":"https://github.com/planetarian/BetterDiscordPlugins","github_raw":"https://raw.githubusercontent.com/planetarian/BetterDiscordPlugins/master/WhoAreYou.plugin.js"},"changelog":[{"title":"0.3.6","items":["Additional fixes for new discord update"]},{"title":"0.3.5","items":["Fix for new discord update"]}],"main":"index.js"};
 
     return !global.ZeresPluginLibrary ? class {
         constructor() {this._config = config;}
@@ -103,7 +103,7 @@ var WhoAreYou = (() => {
                 var groups;
                 if (added.matches(".da-groupStart"))
                     groups = [added];
-                else if (added.matches(".da-chatContent"))
+                else if (added.matches(".da-chatContent") || added.matches('.da-clickableHeader'))
                     groups = added.findAll('.da-groupStart');
                 else return;
                 
