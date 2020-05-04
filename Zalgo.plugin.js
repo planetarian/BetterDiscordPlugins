239d286c74443cec24550877c0a4b5b162a18030
Ash Wolford <piro@live.com>
2020-05-03 19:13:02 -0500

    Updated Zalgo plugin to use new method

--- Zalgo.plugin.js
+++ Zalgo.plugin.js
@@ -24,7 +24,7 @@
 @else@*/
 
 var Zalgo = (() => {
-    const config = {"info":{"name":"Zalgo","authors":[{"name":"Chami","discord_id":"165709167095578625","github_username":"planetarian","twitter_username":"pir0zhki"}],"version":"0.3.0","description":"Zalgo text generation plugin -- write something {{like this}} to corrupt it l̕i̸̶͜ḱ͟e͏̶͢ ̨̛t̢̛҉̧ḩ͘i͘̕͏́͟ş̸̢͘͏\r\nYou can configure the amount of corruption in settings, or prefix it with a corruption amount:\r\n{{0.01:just a little corrupt}} -> j̨ųs͏t̨ ̷a͘ ̸l̶i̷t̀t҉l͡e҉ ̴c̡o͏r҉ŕu̡p̢t̕\r\nYou can also ramp the corruption amount gradually:\r\n{{r:start at zero and get more corrupted}} -> st̶a̷r̸t͜ ҉a̴t̡ ͘z̢e̵r̵o͡ ͝a̡ńd̡ ̛g͝e͞t͏̷ ͜m͟ó̡r̕͠e̸̴ ҉̨͟c̨̀͢͠ơ̕̕͝͞r̸̵̡͢ŕ̛͞u̧p̨͟͝t̴̶͝e̷̡d͏̴́͡","github":"https://github.com/planetarian/BetterDiscordPlugins","github_raw":"https://raw.githubusercontent.com/planetarian/BetterDiscordPlugins/master/Zalgo.plugin.js"},"changelog":[{"title":"0.3.0","items":["Fixed breakage caused by discord update","Switched from locally-tracked classes to use DiscordSelectors"]},{"title":"Plugin revamp","items":["Switched to new plugin format","Switched to new ZeresLib"]}],"main":"index.js"};
+    const config = {"info":{"name":"Zalgo","authors":[{"name":"Chami","discord_id":"165709167095578625","github_username":"planetarian","twitter_username":"pir0zhki"}],"version":"0.4.0","description":"Zalgo text generation plugin -- write something {{like this}} to corrupt it l̕i̸̶͜ḱ͟e͏̶͢ ̨̛t̢̛҉̧ḩ͘i͘̕͏́͟ş̸̢͘͏\r\nYou can configure the amount of corruption in settings, or prefix it with a corruption amount:\r\n{{0.01:just a little corrupt}} -> j̨ųs͏t̨ ̷a͘ ̸l̶i̷t̀t҉l͡e҉ ̴c̡o͏r҉ŕu̡p̢t̕\r\nYou can also ramp the corruption amount gradually:\r\n{{r:start at zero and get more corrupted}} -> st̶a̷r̸t͜ ҉a̴t̡ ͘z̢e̵r̵o͡ ͝a̡ńd̡ ̛g͝e͞t͏̷ ͜m͟ó̡r̕͠e̸̴ ҉̨͟c̨̀͢͠ơ̕̕͝͞r̸̵̡͢ŕ̛͞u̧p̨͟͝t̴̶͝e̷̡d͏̴́͡","github":"https://github.com/planetarian/BetterDiscordPlugins","github_raw":"https://raw.githubusercontent.com/planetarian/BetterDiscordPlugins/master/Zalgo.plugin.js"},"changelog":[{"title":"0.4.0","items":["Fixed breakage caused by discord update","Switched from element manipulation to method patching"]},{"title":"0.3.0","items":["Fixed breakage caused by discord update","Switched from locally-tracked classes to use DiscordSelectors"]},{"title":"Plugin revamp","items":["Switched to new plugin format","Switched to new ZeresLib"]}],"main":"index.js"};
 
     return !global.ZeresPluginLibrary ? class {
         constructor() {this._config = config;}
@@ -41,7 +41,7 @@ var Zalgo = (() => {
             ModalStack.push(function(props) {
                 return BdApi.React.createElement(ConfirmationModal, Object.assign({
                     header: title,
-                    children: [TextElement({color: TextElement.Colors.PRIMARY, children: [`The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`]})],
+                    children: [BdApi.React.createElement(TextElement, {color: TextElement.Colors.PRIMARY, children: [`The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`]})],
                     red: false,
                     confirmText: "Download Now",
                     cancelText: "Cancel",
@@ -59,7 +59,7 @@ var Zalgo = (() => {
     } : (([Plugin, Api]) => {
         const plugin = (Plugin, Library) => {
 
-    const { Logger, Patcher, Settings } = Library;
+    const { Logger, DiscordModules, Patcher, Settings } = Library;
 
     return class Zalgo extends Plugin {
         constructor() {
@@ -112,28 +112,56 @@ var Zalgo = (() => {
 
         onStart() {
             Logger.log("Started");
+            
+            Patcher.before(DiscordModules.MessageActions, "sendMessage", (t,a) => {
+                let content = a[1].content;
+                // Markup format:
+                // {{<o/b>,<r><rampEnd>,<startAmount>-<endAmount>:text}}
+                // o: obscure
+                // b: beneath (don't obscure)
+                // r: enable ramping
+                // rampEnd: normalized value (0.0-1.0) representing ramp length, requires <r>
+                // startAmount: corruption amount at start of ramp; enables ramping even without <r>
+                // endamount: corruption amount at end of ramp, or across whole string if no ramp
+                // text: text to apply zalgo corruption to
+                let regex = /\{\{(?:(?:(?:(o|b))?,?)?(?:(r)(\d+(?:\.\d+)?)?,?)?(?:(\d+(?:\.\d+)?)-)?(\d+(?:\.\d+)?)?\:)?((?:(?!{{).)*?)\}\}/g;
+                if (regex.test(content)) {
+                    content = content.replace(regex, this.doZalgo.bind(this));
+                    if (content.length > 2000) {
+                        PluginUtilities.showToast("This message would exceed the 2000-character limit.\nReduce corruption amount or shorten text.\n\nLength including corruption: " + value.length, {type: 'error'});
+                        e.preventDefault();
+                        return;
+                    }
+                    a[1].content = content;
+                }
+            });
+
             this.update();
         }
 
         onStop() {
-            let textArea = this.getChatTextArea();
-            if (textArea) textArea.off("keydown.zalgo");
-            Logger.log("Stopped");
+            /// Using patch method for now
+            //let textArea = this.getChatTextArea();
+            //if (textArea) textArea.off("keydown.zalgo");
             Patcher.unpatchAll();
+            Logger.log("Stopped");
         }
 
         getSettingsPanel() {
             return Settings.SettingPanel.build(this.saveSettings.bind(this),
-                new Settings.Slider("Corruption amount", "adjust how corrupted your text becomes",
-                    0.05, 3.0, this.settings.corruptionAmount, (e) => { this.settings.corruptionAmount = e; }),
-                new Settings.Slider("Ramp end position", "adjust the endpoint of the ramp-in when using the `r` prefix",
-                    0.05, 1.0, this.settings.rampEnd, e => { this.settings.rampEnd = e; }),
-                new Settings.Switch("Obscure text", "determines whether zalgo characters are placed over the text or beneath it",
-                    this.settings.corruptMid, e => { this.settings.corruptMid = e; })
+                new Settings.SettingGroup("Zalgo Settings", {collapsible: false, shown: true}).append(
+                    new Settings.Slider("Corruption amount", "Adjusts how corrupted your text becomes",
+                        0.05, 3.0, this.settings.corruptionAmount, (e) => { this.settings.corruptionAmount = e; }),
+                    new Settings.Slider("Ramp end position", "Adjusts the endpoint of the ramp-in when using the `r` prefix",
+                        0.05, 1.0, this.settings.rampEnd, e => { this.settings.rampEnd = e; }),
+                    new Settings.Switch("Obscure text", "Determines whether zalgo characters are placed over the text or beneath it (use the `o` or `b` prefixes to set this in-line)",
+                        this.settings.corruptMid, e => { this.settings.corruptMid = e; })
+                )
             );
         }
         
         observer({ addedNodes, removedNodes }) {
+            /* /// unneeded due to patch method
             if (!this.classes || !addedNodes || !addedNodes[0] || !addedNodes[0].classList) return;
             let node = addedNodes[0];
 
@@ -141,16 +169,20 @@ var Zalgo = (() => {
             if (node.matches(sel.TitleWrap.chat)
                 || node.matches(sel.TitleWrap.chatContent)) {
                 this.update();
-            }
+            }*/
         }
 
         update() {
+
+            /* /// Using patching for now; tab completion pending further testing
             let textArea = this.getChatTextArea();
             if (!textArea || !textArea.length) return;
 
             let inputBox = textArea[0];
+            Logger.log(inputBox);
             textArea.off("keydown.zalgo").on("keydown.zalgo", (e) => {
                 // Corrupt text either when we press enter or tab-complete
+                Logger.log(e.which);
                 if ((e.which == 13 || e.which == 9) && inputBox.value) {
                     let cursorPos = inputBox.selectionEnd;
                     let value = inputBox.value;
@@ -186,20 +218,24 @@ var Zalgo = (() => {
                     }
                 }
             });
+            */
             
             this.initialized = true;
         }
 
         getChatTextArea() {
-            let sel = ZLibrary.DiscordSelectors;
-            return $(sel.Textarea.channelTextArea.value + " " + sel.Textarea.textArea.value);
+            return $(".slateTextArea-1bp44y");
+            //let sel = ZLibrary.DiscordSelectors;
+            //return $(sel.Textarea.channelTextArea.value + " " + sel.Textarea.textArea.value);
         }
 
-        doZalgo(match, ramp, rampEnd, startAmt, endAmt, contents, offset, string) {
+        doZalgo(match, midMode, ramp, rampEnd, startAmt, endAmt, contents, offset, string) {
             const maxAmt = 10;
             let hasStart = startAmt >= 0 && startAmt <= maxAmt;
             let hasEnd = endAmt >= 0 && endAmt <= maxAmt;
             let hasRampEnd = rampEnd >= 0 && rampEnd <= 1;
+
+            let corruptMid = midMode == "o" || (midMode != "b" && this.settings.corruptMid);
             let doRamp = ramp == "r" || hasStart;
 
             if (!hasRampEnd) rampEnd = this.settings.rampEnd;
@@ -210,10 +246,10 @@ var Zalgo = (() => {
             // otherwise, start at 0 if we are ramping but no start provided
             else if (!hasStart) startAmt = 0;
 
-            return this.getZalgo(contents, parseFloat(rampEnd), parseFloat(startAmt), parseFloat(endAmt));
+            return this.getZalgo(contents, corruptMid, parseFloat(rampEnd), parseFloat(startAmt), parseFloat(endAmt));
         }
 
-        getZalgo(txt, rampEnd, startAmt, endAmt) {
+        getZalgo(txt, corruptMid, rampEnd, startAmt, endAmt) {
             //============================================================
             // ZALGO text script by tchouky, adapted/modified by Chami
             // See original at http://eeemo.net/
@@ -222,7 +258,7 @@ var Zalgo = (() => {
             // Get saved options for corruption directions
             let config = this.settings;
             let optUp = config.corruptUp;
-            let optMid = config.corruptMid;
+            let optMid = corruptMid;
             let optDown = config.corruptDown || (!optUp && !optMid);
 
             let newTxt = "";
